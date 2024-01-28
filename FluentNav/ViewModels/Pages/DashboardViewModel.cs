dc8fece630cfd9b57e9adbd135b5dc5a3cc18a14
sosuisen <hidekazu.kubota@gmail.com>
2024-01-29 00:46:56 +0900

    fix: add practice2

--- FluentNav/ViewModels/Pages/DashboardViewModel.cs
+++ FluentNav/ViewModels/Pages/DashboardViewModel.cs
@@ -1,7 +1,11 @@
-﻿// This Source Code Form is subject to the terms of the MIT License.
+// This Source Code Form is subject to the terms of the MIT License.
 // If a copy of the MIT was not distributed with this file, You can obtain one at https://opensource.org/licenses/MIT.
 // Copyright (C) Leszek Pomianowski and WPF UI Contributors.
 // All Rights Reserved.
+
+using FluentNav.ViewModels.Windows;
+using Wpf.Ui.Common;
+using Wpf.Ui.Controls;
 
 namespace FluentNav.ViewModels.Pages
 {
@@ -9,11 +13,27 @@ namespace FluentNav.ViewModels.Pages
     {
         [ObservableProperty]
         private int _counter = 0;
+        
+        private readonly ISnackbarService _snackbarService;
+        private readonly IContentDialogService _contentDialogService;
 
-        [RelayCommand]
+        public DashboardViewModel(
+            ISnackbarService snackbarService,
+            IContentDialogService contentDialogService
+        )
+        {
+            _snackbarService = snackbarService;
+            _contentDialogService = contentDialogService;
+        }
+
+            [RelayCommand]
         private void OnCounterIncrement()
         {
             Counter++;
+            // _snackbarService.Show("Notice", "Counter incremented.");
+            // _snackbarService.Show("Notice", "Counter incremented.", ControlAppearance.Danger);
+
+            // _contentDialogService.ShowAlertAsync("Notice", "Counter incremented.", "Close");
         }
     }
 }
